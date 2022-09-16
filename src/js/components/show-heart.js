7a37109c5d0d6e936ca1d9ad44c710aa2992c390
Fredfed-89 <fedyaev.sergei89@gmail.com>
2022-09-16 12:32:20 +0300

    little work with filters

---
+++ src/js/components/show-heart.js
@@ -0,0 +1,5 @@
+const full = document.querySelector('#full');
+
+full.addEventListener('click', function () {
+  full.classList.toggle('btn-favorite-full');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,4 @@ import './_components';
 import './components/filters';
 import './components/openFilters';
 import './components/hamburger';
+import './components/show-heart';
